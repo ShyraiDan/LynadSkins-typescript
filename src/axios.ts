bbe52eacc1ff731d599d036fc1bb4950804a4a4c
ShirayDan <dan.shirayy@gmail.com>
2023-08-25 20:33:51 +0300

    changing url

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4444/',
-	//baseURL: process.env.REACT_APP_API_URL,
+	//baseURL: 'http://localhost:4444/',
+	baseURL: process.env.REACT_APP_API_URL,
 })
 
 instance.interceptors.request.use((config) => {
