26176144cdfdd29613a5a01fb31f9184b51ffd7f
ShirayDan <dan.shirayy@gmail.com>
2024-02-11 16:40:52 +0200

    added theme changing for search result

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:5555/'
+  baseURL: 'http://localhost:5555/'
   // baseURL: process.env.REACT_APP_API_URL
 })
 
